08cc63f744a68c0c873847652d16a6742f8a011d
thejohnyw <john.y.wang.26@dartmouth.edu>
2022-10-17 18:43:57 -0400

    first

---
+++ README.md
@@ -0,0 +1,42 @@
+Discord Bot that generates NASA Astronomy Pic of the Day (APOD) and posts to Reddit upon mention!
+
+INSTRUCTIONS IN TERMINAL
+
+
+1.
+  
+    git clone https://github.com/thejohnyw/Discord-NASA-Reddit-Bot.git
+
+2.
+  
+    cd Downloads/Discord-NASA-Reddit-Bot
+
+3.
+
+    
+    pip3 install -r requirements.txt
+
+4.
+
+    python3 discord-nasa-reddit-bot.py
+ OR run file in IDE: discord-nasa-reddit-bot.py
+
+5. 
+    login into discord and join this server link for demo: https://discord.gg/EFeGRYD496
+
+you're welcome to use this discord account:
+username: bot_tester#0350
+password: dali_is_cool789
+
+bot instructions:
+mention bot (@nasa_bot) then type the command as shown.
+
+  For a random Astronomy picture and description, "@nasa_bot random"
+        
+  For the picture of the day, "@nasa_bot today"
+        
+  For a specific date, "@nasa_bot date: year-month-day"
+        
+  If you really like the APOD, post it to reddit using "@nasa_bot post"
+
+reddit account that shows posts: https://www.reddit.com/user/nasadiscordredditbot
---
+++ discord-nasa-reddit-bot.py
@@ -0,0 +1,116 @@
+'''
+Author: John Wang
+October 2022
+Purpose: Discord Bot that sends NASA pictures and posts them to Reddit upon mention!
+'''
+import discord #Discord API
+import os
+import random
+from dotenv import load_dotenv
+import requests
+from datetime import date
+import praw #Reddit API wrapper
+
+#os and dotenv are used to privately access password and API keys
+load_dotenv()
+discord_token = os.getenv('DISCORD_TOKEN')
+nasa_key = os.getenv('NASA_KEY')
+reddit_client = os.getenv('REDDIT_CLIENT')
+reddit_secret = os.getenv('REDDIT_SECRET')
+reddit_pass = os.getenv('REDDIT_PASS')
+
+
+
+#function to post pic to subreddit APOD: https://www.reddit.com/r/apod/, and comment description under the post
+def POST(description, pic):
+    post = reddit.subreddit('apod').submit('User Generated APOD', url=pic)
+    post.reply(body = description)
+
+#connects with Reddit API
+reddit = praw.Reddit(client_id = reddit_client, client_secret = reddit_secret,
+    user_agent = 'nasa-discord bot', username = 'nasadiscordredditbot', password = reddit_pass, check_for_async = False)
+
+posts = [] #temporary memory of what discord bot sends so that it can post the most recent one to reddit
+
+
+
+#generate a random date for NASA Astronomy pic
+def randomdate():
+    month, year = random.randint(1,12), random.randint(1995,2021)
+    days_in_month = {1:31, 2:28, 3:31, 4:29, 5:31, 6:30, 7:29, 8:31, 9:30, 10:31, 11:30, 12:30}
+    if (year%4 == 0 and year%100 != 0) or (year%400 == 0): #leap year consideration
+        days_in_month[2] = 29
+    day = random.randint(1,days_in_month[month])
+    date = str(year)+'-'+str(month)+'-'+str(day)
+    return date
+
+#calls NASA API for picture on given date; returns picture and its description 
+def nasa_info(option, t_date=None): #parameters: option is what date you want: a random one, specific one, or today's date
+    #--> t_date is optional parameter, used for specific date
+    options_dict = {'random': randomdate(), 'specific': t_date, 'today': date.today()}
+    given_date = options_dict.get(option)
+    call = 'https://api.nasa.gov/planetary/apod?date={}&hd=True&api_key={}'.format(given_date, nasa_key) #NASA APOD API
+    r = requests.get(call)
+    return r.json()["explanation"], \
+        r.json()["hdurl"]
+
+
+
+#creates client to connect with Discord API
+#using all intents because accessing user message contents requires it
+client = discord.Client(intents=discord.Intents.all()) 
+
+botID = '<@1030953607073378415>' #allows for users to mention bot using @
+
+#event when bot ready: print to console that it's ready
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+
+#event for when new member joins a server that is using the bot: it sends a dm about possible bot commands
+@client.event
+async def on_member_join(member):
+    await member.send(
+        'Welcome! I am NASA/Reddit Bot! To use my commands, mention me then type the command as shown. \n' +
+        'For a random Astronomy picture and description, "@nasa_bot random" \n' +
+        'For the picture of the day, "@nasa_bot today" \n' +
+        'For a specific date, "@nasa_bot date: year-month-day" \n' +
+        'If you really like the APOD, post it to reddit using "@nasa_bot post"'
+    )
+
+#event for user sending a message/command
+@client.event
+async def on_message(message): #parameter: user's message
+    global posts #will append bot's NASA messages to posts list
+    
+    if message.author == client.user or not message.content.startswith(botID): #checks for user message and makes sure discord bot won't message unless it is mentioned
+        return
+
+    if message.content == botID + " random":
+        descript, pic = nasa_info('random')
+        bot_reply = descript + "\n\n{}\n\n*".format(pic)
+        posts.append(bot_reply)
+        posts.append(pic)
+    elif message.content == botID + " today":
+        descript, pic = nasa_info('today')
+        bot_reply = descript + "\n\n{}\n\n*".format(pic)
+        posts.append(bot_reply)
+        posts.append(pic)
+    elif message.content.startswith(botID + " date: "):
+        descript, pic = nasa_info('specific', t_date=str(message.content[len(botID)+7::])) #takes t_date parameter as user input of numbers after "date: "
+        bot_reply = descript + "\n\n{}\n\n*".format(pic)
+        posts.append(bot_reply)
+        posts.append(pic)
+    elif message.content == botID + " post":
+        if posts:
+            POST(posts[-2], posts[-1]) #post most recent nasa message from bot to Reddit
+            bot_reply = 'Successfully posted to reddit!: https://www.reddit.com/user/nasadiscordredditbot '
+        else: #if user tries to post, but there's no bot-generated NASA APOD yet
+            bot_reply = 'Error: No previous APOD to post'
+    else: #if user mentions bot but doesn't give any of above commands
+        bot_reply = 'Invalid command: command not found'
+
+    await message.channel.send(bot_reply)
+    
+        
+client.run(discord_token)
